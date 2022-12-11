f4bafb2db5e77815e0a2fe08455963301daf9336
itisyijy <jy02060577@gmail.com>
2022-12-11 16:32:34 +0900

    Final

--- src/hierarchy.java
+++ src/hierarchy.java
@@ -19,8 +19,6 @@ public class hierarchy {
     static float L3_hit = 0.0f;
     static float L4_access = 0.0f;
     static float L4_hit = 0.0f;
-    static float Disk_hit = 0.0f;
-    static float Disk_access = 0.0f;
     public static float L1_hitRatio() {
         return (L1_hit / L1_access);
     }
@@ -32,9 +30,6 @@ public class hierarchy {
     }
     public static float L4_hitRatio() {
         return (L4_hit / L4_access);
-    }
-    public static float Disk_hitRatio() {
-        return (Disk_hit / Disk_access);
     }
     public static void main(String[] args) {
 
@@ -91,7 +86,7 @@ public class hierarchy {
             random = r.nextInt(total_data / 4);
             if (!used.contains(random)) {
                 used.add(random);
-                L4.add(Disk.get(random * 4 + 0));
+                L4.add(Disk.get(random * 4));
                 L4.add(Disk.get(random * 4 + 1));
                 L4.add(Disk.get(random * 4 + 2));
                 L4.add(Disk.get(random * 4 + 3));
@@ -113,7 +108,7 @@ public class hierarchy {
             random = r.nextInt(L4_size / 2);
             if (!used.contains(random)) {
                 used.add(random);
-                L3[i].add(0, L4.get(random * 2 + 0));
+                L3[i].add(0, L4.get(random * 2));
                 L3[i].add(1, L4.get(random * 2 + 1));
             }
             else
@@ -142,8 +137,21 @@ public class hierarchy {
         L1.add(L2.get(random));
 //        System.out.println("\n" + L1.get(0).block.price + " valid:" + L1.get(0).valid + " tag:" + L1.get(0).tag);
 
-        // SAMPLE INPUTS TO MEASURE MISS RATIO
-        String[] sample_date = {"2021-12-28", "2021-11-22", "2021-10-21", "2021-09-23", "2021-08-11", "2012-11-02", "2012-10-25", "2012-09-20", "2012-08-16", "2012-07-26"};
+        // SAMPLE INPUTS TO MEASURE MISS RATE
+        String[] sample_date = {"1997-12-12", "1997-12-11", "1997-12-10", "1997-12-09",
+                "1997-12-08", "1997-12-05", "1997-12-04", "1997-12-03", "1997-12-02",
+                "1997-12-01", "1997-11-28", "1997-11-27", "1997-11-26", "1997-11-25",
+                "1997-11-24", "1997-11-21", "1997-11-20", "1997-11-19", "1997-11-18",
+                "1997-11-17", "1997-11-14", "1997-11-13", "1997-11-12", "2008-03-13",
+                "2008-03-12", "2008-03-11", "2008-03-10", "2008-03-07", "2008-03-06",
+                "2008-03-05", "2008-03-04", "2008-03-03", "2008-02-29", "2008-03-13",
+                "2008-03-12", "2008-03-11", "2008-03-10", "2008-03-18", "2008-03-17",
+                "2008-03-13", "2008-03-12", "2008-03-11", "2008-03-10", "2021-12-31",
+                "1999-07-07", "1981-05-12", "2002-01-21", "2011-08-16", "2018-11-06",
+                "2009-11-11", "1987-10-06", "2019-02-20", "1991-07-31", "2020-12-03",
+                "1995-06-21", "2008-03-13", "2008-03-13", "2008-03-13", "2008-03-13",
+                "1981-05-12", "1981-05-12", "1981-05-12", "1981-05-12", "1981-05-12",
+                "1981-05-12",  "2021-12-31", "2021-12-31"};
         int[] samples = new int[sample_date.length];
 
         for(int i=0; i < sample_date.length; i++)
@@ -153,23 +161,23 @@ public class hierarchy {
             int seek;
 
             /* TO-DO
-                COPY TO UPPER CLASS ??
+                COPY TO UPPER CLASS
                 - 특정한 레벨의 캐쉬에서 데이터를 찾으면 해당 레벨 캐쉬에 있는 모든 상위 레벨의 캐쉬에 찾은 값을 복사해 넣는다.
                 - L1을 제외하고는 2-4개 정도의 주변 데이터도 함께 복사해간다.
-
-                # OF ACCESS & # OF HIT
-                - 접근을 시도할 때마다 ACCESS COUNT
-                - 데이터를 찾을 때마다 HIT COUNT
+                - 참조된 캐쉬는 해당 캐쉬의 맨 뒤로 이동한다.
              */
 
             // SEEK SAMPLE IN L1
+            L1_access++;
             seek = 0;
             if (sample == L1.get(seek).tag) {
-                System.out.println("Find: ₩" + L1.get(seek).block.price);
+                L1_hit++;
+                System.out.println("In L1,\t Date:(" + sample + ") = ₩ " + L1.get(seek).block.price);
                 continue;
             }
 
             // SEEK SAMPLE IN L2
+            L2_access++;
             seek = 0;
             while (sample != L2.get(seek).tag) {
                 seek++;
@@ -178,13 +186,18 @@ public class hierarchy {
             }
 
             if (seek < L2_size) {
-                System.out.println("Find: ₩" + L2.get(seek).block.price);
-                // copy to upper level
-
+                L2_hit++;
+                System.out.println("In L2,\t Date:(" + sample + ") = ₩ " + L2.get(seek).block.price);
+                // COPY TO UPPER CACHE: L2 -> L2, L1
+                L1.remove(0);
+                L1.add(L2.get(seek));
+                L2.add(L2.get(seek));
+                L2.remove(seek);
                 continue;
             }
 
             // SEEK SAMPLE IN L3
+            L3_access++;
             seek = 0;
             while (sample != L3[seek].get(0).tag && sample != L3[seek].get(1).tag) {
                 seek++;
@@ -193,16 +206,46 @@ public class hierarchy {
             }
 
             if (seek < L3_size1) {
-                if (sample == L3[seek].get(0).tag)
-                    System.out.println("Find: ₩" + L3[seek].get(0).block.price);
-                else
-                    System.out.println("Find: ₩" + L3[seek].get(1).block.price);
-                // copy to upper level
+                L3_hit++;
+                if (sample == L3[seek].get(0).tag) {
+                    System.out.println("In L3\t Date:(" + sample + ") = ₩ " + L3[seek].get(0).block.price);
+                    L1.add(L3[seek].get(0));
+                }
+                else {
+                    System.out.println("In L3,\t Date:(" + sample + ") = ₩ " + L3[seek].get(1).block.price);
+                    L1.add(L3[seek].get(1));
+                }
+                // COPY TO UPPER CACHE: L3 -> L3, L2, L1
+                L1.remove(0);
 
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+                L2.add(L3[seek].get(0));
+                L2.add(L3[seek].get(1));
+                if (seek == L3_size1 - 1) {
+                    L2.add(L3[seek - 1].get(0));
+                    L2.add(L3[seek - 1].get(1));
+                }
+                else {
+                    L2.add(L3[seek + 1].get(0));
+                    L2.add(L3[seek + 1].get(1));
+                }
+
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+                L3[L3_size1 - 1].set(0, L2.get(L2_size - 1));
+                L3[L3_size1 - 1].set(1, L2.get(L2_size - 2));
+                L3[L3_size1 - 2].set(0, L2.get(L2_size - 3));
+                L3[L3_size1 - 2].set(1, L2.get(L2_size - 4));
                 continue;
             }
 
             // SEEK SAMPLE IN L4
+            L4_access++;
             seek = 0;
             while (sample != L4.get(seek).tag) {
                 seek++;
@@ -211,9 +254,59 @@ public class hierarchy {
             }
 
             if (seek < L4_size) {
-                System.out.println("Find: ₩" + L4.get(seek).block.price);
-                // copy to upper level
+                L4_hit++;
+                System.out.println("In L4,\t Date:(" + sample + ") = ₩ " + L4.get(seek).block.price);
+                // COPY TO UPPER CACHE: L4 -> L4, L3, L2, L1
+                L1.add(L4.get(seek));
+                L1.remove(0);
 
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+
+                if (seek < L4_size - 4) {
+                    L2.add(L4.get(seek));
+                    L2.add(L4.get(seek + 1));
+                    L2.add(L4.get(seek + 2));
+                    L2.add(L4.get(seek + 3));
+
+                    L3[L3_size1 - 1].set(0, L4.get(seek));
+                    L3[L3_size1 - 1].set(1, L4.get(seek + 1));
+                    L3[L3_size1 - 2].set(0, L4.get(seek + 2));
+                    L3[L3_size1 - 2].set(1, L4.get(seek + 3));
+
+                    L4.add(L4.get(seek));
+                    L4.add(L4.get(seek + 1));
+                    L4.add(L4.get(seek + 2));
+                    L4.add(L4.get(seek + 3));
+                }
+                else {
+                    L4.add(L4.get(seek));
+                    L4.add(L4.get(seek - 1));
+                    L4.add(L4.get(seek - 2));
+                    L4.add(L4.get(seek - 3));
+
+                    L3[L3_size1 - 1].set(0, L4.get(seek));
+                    L3[L3_size1 - 1].set(1, L4.get(seek - 1));
+                    L3[L3_size1 - 2].set(0, L4.get(seek - 2));
+                    L3[L3_size1 - 2].set(1, L4.get(seek - 3));
+
+                    L4.add(L4.get(seek));
+                    L4.add(L4.get(seek - 1));
+                    L4.add(L4.get(seek - 2));
+                    L4.add(L4.get(seek - 3));
+                }
+
+                L4.remove(0);
+                L4.remove(0);
+                L4.remove(0);
+                L4.remove(0);
                 continue;
             }
 
@@ -226,22 +319,68 @@ public class hierarchy {
             }
 
             if (seek < Disk.size()) {
-                System.out.println("Find: ₩" + Disk.get(seek).block.price);
-                // copy to upper level
+                System.out.println("In Disk, Date:(" + sample + ") = ₩ " + Disk.get(seek).block.price);
+                // COPY TO UPPER CACHE: Disk -> L4, L3, L2, L1
+                L1.add(Disk.get(seek));
+                L1.remove(0);
 
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+                L2.remove(0);
+
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+                for (int i = 0; i < L3_size1 - 1; i++)
+                    L3[i] = L3[i + 1];
+
+                if (seek < Disk.size() - 4) {
+                    L2.add(Disk.get(seek));
+                    L2.add(Disk.get(seek + 1));
+                    L2.add(Disk.get(seek + 2));
+                    L2.add(Disk.get(seek + 3));
+
+                    L3[L3_size1 - 1].set(0, Disk.get(seek));
+                    L3[L3_size1 - 1].set(1, Disk.get(seek + 1));
+                    L3[L3_size1 - 2].set(0, Disk.get(seek + 2));
+                    L3[L3_size1 - 2].set(1, Disk.get(seek + 3));
+
+                    L4.add(Disk.get(seek));
+                    L4.add(Disk.get(seek + 1));
+                    L4.add(Disk.get(seek + 2));
+                    L4.add(Disk.get(seek + 3));
+                }
+                else {
+                    L4.add(Disk.get(seek));
+                    L4.add(Disk.get(seek - 1));
+                    L4.add(Disk.get(seek - 2));
+                    L4.add(Disk.get(seek - 3));
+
+                    L3[L3_size1 - 1].set(0, Disk.get(seek));
+                    L3[L3_size1 - 1].set(1, Disk.get(seek - 1));
+                    L3[L3_size1 - 2].set(0, Disk.get(seek - 2));
+                    L3[L3_size1 - 2].set(1, Disk.get(seek - 3));
+
+                    L4.add(Disk.get(seek));
+                    L4.add(Disk.get(seek - 1));
+                    L4.add(Disk.get(seek - 2));
+                    L4.add(Disk.get(seek - 3));
+                }
+
+                L4.remove(0);
+                L4.remove(0);
+                L4.remove(0);
+                L4.remove(0);
+                System.out.println();
             }
 
         }
+
         // PRINT OUT MISS RATIO OR HIT RATIO
-        System.out.println("L1 Cache HIT Ratio: " + L1_hitRatio());
-        System.out.println("L1 Cache Miss Ratio: " + (1.0f - L1_hitRatio()));
-        System.out.println("L2 Cache HIT Ratio: " + L2_hitRatio());
-        System.out.println("L2 Cache Miss Ratio: " + (1.0f - L2_hitRatio()));
-        System.out.println("L3 Cache HIT Ratio: " + L3_hitRatio());
-        System.out.println("L3 Cache Miss Ratio: " + (1.0f - L3_hitRatio()));
-        System.out.println("L4 Cache HIT Ratio: " + L4_hitRatio());
-        System.out.println("L4 Cache Miss Ratio: " + (1.0f - L4_hitRatio()));
-        System.out.println("Disk Cache HIT Ratio: " + Disk_hitRatio());
-        System.out.println("Disk Cache Miss Ratio: " + (1.0f - Disk_hitRatio()));
+        System.out.println();
+        System.out.println("L1 Cache Miss Rate : " + (1.0f - L1_hitRatio()) * 100.0f + "%");
+        System.out.println("L2 Cache Miss Rate : " + (1.0f - L2_hitRatio()) * 100.0f + "%");
+        System.out.println("L3 Cache Miss Rate : " + (1.0f - L3_hitRatio()) * 100.0f + "%");
+        System.out.println("L4 Cache Miss Rate : " + (1.0f - L4_hitRatio()) * 100.0f + "%");
     }
 }
